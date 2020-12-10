9c086ef74c87a72e009a81342338fd122d3f1810
subhankarCN <subhankar.ray@capitalnumbers.com>
2020-12-10 11:18:39 +0530

    check version api for app end

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,12 @@ router.get('/checkVersion', (req,res)=>{
     android: {
       version: '0.0.1',
       storeUrl: 'https://play.google.com/store/apps/details?id=com.AskTeddi.Solutions4Health',
+      forceUpdate: true
     },
     ios: {
       version: '1.0',
       storeUrl: 'https://apps.apple.com/us/app/id1531123323',
+      forceUpdate: false
     },
   }
   res.json(payload)
