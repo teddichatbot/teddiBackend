6fa0709498e903fdf53c0bbd4e2a9af691e84722
ashraf-capital <ashraf@capitalnumbers.com>
2021-11-18 20:09:28 +0530

    version updated

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ router.get('/', function(req, res, next) {
 router.get('/checkVersion', (req,res)=>{
   let payload = {
     android: {
-      version: '1.0',
+      version: '1.1',
       storeUrl: 'market://details?id=com.AskTeddi.Solutions4Health',
       forceUpdate: true
     },
     ios: {
-      version: '1.0',
+      version: '1.1',
       storeUrl: 'itms-apps://apps.apple.com/us/app/id1531123323',
       forceUpdate: true
     },
