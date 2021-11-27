c9bac3374e5a6c91c5e65b4c94ccfb7a893ae8b8
ashraf-capital <ashraf@capitalnumbers.com>
2021-11-27 19:12:23 +0530

    console removed

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home  page.*/
+//test deploymnet
 router.get('/', function(req, res, next) {
-  console.log("Hello");
   res.render('index', { title: 'Express' });
 });
 router.get('/checkVersion', (req,res)=>{
