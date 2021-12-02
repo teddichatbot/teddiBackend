78d3c250487f8fdae015c1e13b88f3575d09e4a7
teddichatbot <teddichatbot@gmail.com>
2021-12-02 13:38:11 +0530

    commit modified

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home  page.*/
-//test deploymnet
+//test deploymnet 2
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
